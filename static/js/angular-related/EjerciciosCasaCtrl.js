31bb447f0288c14bd4b5e7ad9a55620291b5b4c2
Jose Enrique Carrillo Pino <quique0194@gmail.com>
2015-06-12 12:55:42 -0500

    Added some example exercises

--- static/js/angular-related/EjerciciosCasaCtrl.js
+++ static/js/angular-related/EjerciciosCasaCtrl.js
@@ -1,17 +1,19 @@
 angular.module('app').
     controller('EjerciciosCasaCtrl', [
-        '$scope', '$rootScope',
-        function ($scope, $rootScope) {
+        '$scope', '$rootScope', '$timeout',
+        function ($scope, $rootScope, $timeout) {
             $rootScope.setCurTab($rootScope.tabs[1]);
 
             $scope.init = function () {
-                var gridster = $(".gridster ul").gridster({
-                    widget_base_dimensions: [100, 120],
-                    widget_margins: [5, 5],
-                    draggable: {
-                      handle: 'header'
-                    }
-                }).data('gridster');
+                $timeout(function () {
+                    var gridster = $(".gridster ul").gridster({
+                        widget_base_dimensions: [100, 120],
+                        widget_margins: [5, 5],
+                        draggable: {
+                          handle: 'header'
+                        }
+                    }).data('gridster');
+                }, 100);
             }
 
             $scope.init();
--- static/js/angular-related/root-scope-init.js
+++ static/js/angular-related/root-scope-init.js
@@ -1,24 +1,93 @@
 angular.module('app').
     run(['$rootScope',
         function ($rootScope) {
+            var exercises = [
+                {
+                    name: "Dominadas",
+                    pic: "static/images/exercises/dominadas.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Estiramiento de espalda",
+                    pic: "static/images/exercises/estiramiento-de-espalda.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Estiramiento de gluteos",
+                    pic: "static/images/exercises/estiramiento-de-gluteos.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Estiramiento de pantorrilla",
+                    pic: "static/images/exercises/estiramiento-de-pantorrilla.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Estiramiento de pierna",
+                    pic: "static/images/exercises/estiramiento-de-pierna.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Planchas",
+                    pic: "static/images/exercises/planchas.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Planchas con aplauso",
+                    pic: "static/images/exercises/planchas-con-aplauso.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Sentadillas",
+                    pic: "static/images/exercises/sentadillas.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+                {
+                    name: "Sentadillas con barra",
+                    pic: "static/images/exercises/sentadillas-con-barra.jpg",
+                    series: 3,
+                    reps: 10,
+                    time: 5,
+                },
+            ];
+
             $rootScope.patients = [
                 {
                     name: "Joe Smith",
                     pic: "media/user1.jpg",
                     desease: "Hernia Lumbar L2",
                     day: 6,
+                    exercises: exercises,
                 },
                 {
                     name: "Jane Doe",
                     pic: "media/user2.jpg",
                     desease: "Hernia Lumbar L2",
                     day: 4,
+                    exercises: exercises,
                 },
                 {
                     name: "Axel Rose",
                     pic: "media/user3.jpg",
                     desease: "Hernia Lumbar L2",
                     day: 8,
+                    exercises: exercises,
                 },
             ];
             $rootScope.curPatient = $rootScope.patients[0];
